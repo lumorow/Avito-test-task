4357cdde36fbd0440d12ebcb6cd270bc7e10b189
lumorow <lukazhovtovski@gmail.com>
2023-08-26 18:12:55 +0700

    delete segment

--- src/pkg/postgres/segment.go
+++ src/pkg/postgres/segment.go
@@ -1,19 +1,51 @@
 package postgres
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func (db *Repo) CreateSegment(segment string) (int, error) {
-	createSegment := fmt.Sprintf("INSERT INTO %s (name) values ($1) RETURNING id", segment)
-	var SegmentId int
+func (db *Repo) CreateSegment(segmentName string) (int, error) {
+	createSegment := fmt.Sprintf("INSERT INTO %s (name) values $1 RETURNING id", "segments")
+	var segmentID int
 
-	row, err := db.Db.Query(createSegment, segment)
+	row, err := db.Db.Query(createSegment, segmentName)
 	if err != nil {
 		return 0, err
 	}
 
-	err = row.Scan(&SegmentId)
+	defer row.Close()
+
+	if row.Next() {
+		err = row.Scan(&segmentID)
+		if err != nil {
+			return 0, err
+		}
+	} else {
+		return 0, errors.New("Failed to create segment")
+	}
+
+	return segmentID, nil
+}
+
+func (db *Repo) DeleteSegment(segmentName string) (int, error) {
+	deleteSegment := fmt.Sprintf("DELETE FROM %s WHERE name = $1 RETURNING id", "segments")
+	var segmentID int
+
+	row, err := db.Db.Query(deleteSegment, segmentName)
 	if err != nil {
 		return 0, err
 	}
-	return SegmentId, nil
+	defer row.Close()
+
+	if row.Next() {
+		err = row.Scan(&segmentID)
+		if err != nil {
+			return 0, err
+		}
+	} else {
+		return 0, errors.New("Failed to delete segment")
+	}
+
+	return segmentID, nil
 }
--- src/pkg/router/handlers.go
+++ src/pkg/router/handlers.go
@@ -5,6 +5,7 @@ import (
 	"Avito-test-task/parser"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -12,17 +13,21 @@ func (r *Router) CreateSegmentHandler(c *gin.Context) {
 	segment := &models.Segment{}
 
 	if err := c.ShouldBindJSON(&segment); err != nil {
+		log.Error(err)
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 		return
 	}
+
 	SegmentName, err := parser.ParseSegmentName(segment.SegmentName)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		log.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("%s", err)})
 		return
 	}
 
 	segmentId, err := r.Db.CreateSegment(SegmentName)
 	if err != nil {
+		log.Error(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create segment"})
 		return
 	}
@@ -31,7 +36,16 @@ func (r *Router) CreateSegmentHandler(c *gin.Context) {
 }
 
 func (r *Router) DeleteSegmentHandler(c *gin.Context) {
+	slug := c.Param("slug")
 
+	deletedID, err := r.Db.DeleteSegment(slug)
+	if err != nil {
+		log.Error(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete segment"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Segment deleted successfully", "deleted_id": deletedID})
 }
 
 func (r *Router) AddUserSegmentsHandler(c *gin.Context) {
